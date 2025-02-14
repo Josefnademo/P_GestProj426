504098236495fc54434ec53f8b188f7daf20dba8
Romain <163823947+romaindenis1@users.noreply.github.com>
2025-02-14 17:21:01 +0100

    Setup(env)

--- application/src/app.mjs
+++ application/src/app.mjs
@@ -1,27 +1,26 @@
 import { sequelize, Compte } from "./db/sequelize.mjs";
-
-dotenv.config();
-console.log(process.env);
-
+import express from "express";
+import { app_port } from "./config.mjs";
+import path from "path";
 const app = express();
+app.set("view engine", "ejs");
 
-app.get("/", function (req, res) {
-  //Connection à la DB
-  sequelize
-    .authenticate()
-    .then((_) =>
-      console.log("La connexion à la base de données a bien été établie")
-    )
-    .catch((error) => console.error("Impossible de se connecter à la DB"));
-  res.send(`Hello ${process.env.DBNAME}`);
+app.get("/", (req, res) => {
+  res.render("accueil", { foo: "FOO" });
 });
 
-app.listen(3000, () => {
-  console.log(`Example app listening at http://localhost:3000`);
-import express from "express";
+//Connection à la DB
+sequelize
+  .authenticate()
+  .then((_) =>
+    console.log("La connexion à la base de données a bien été établie")
+  )
+  .catch((error) => console.error("Impossible de se connecter à la DB"));
 
-const app = express();
-const port = 3000;
+app.listen(app_port, () => {
+  console.log(`Example app listening at http://localhost:${app_port}`);
+});
+
 app.use(express.json());
 app.set("view engine", "ejs");
 import { avisRouter } from "./routes/avis.mjs";
@@ -34,21 +33,13 @@ import { lieuRouter } from "./routes/lieu.mjs";
 app.use("/lieu/", lieuRouter);
 
 app.get("/", (req, res) => {
-  res.render("accueil", { name: "antoine" });
+  res.render("accueil");
 });
 /*
 app.get("/", (req, res) => {
   res.redirect(`http://localhost:${port}/accueil/`);
 });*/
-
-app.get("/accueil/", (req, res) => {
-  res.render("accueil", { name: "utilisateur" });
-});
 app.use(({ res }) => {
   const message = "ERREUR 404";
   res.status(404).json(message);
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
 });
