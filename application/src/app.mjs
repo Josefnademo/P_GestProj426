c5d9be96ccebb0b5df54acc34d548dac4d26825c
ASETML <alban.segalen@eduvaud.ch>
2025-03-07 15:17:30 +0100

    supression du globe de app.mjs

--- application/src/app.mjs
+++ application/src/app.mjs
@@ -11,66 +11,7 @@ app.get("/", function (req, res) {
       console.log("La connexion à la base de données a bien été établie")
     )
     .catch((error) => console.error("Impossible de se connecter à la DB"));
-  res.send(`<!DOCTYPE html>
-
-<html lang="en">
-
-<head>
-
-  <meta charset="utf-8">
-
-  <!-- Include the CesiumJS JavaScript and CSS files -->
-
-  <script src="https://cesium.com/downloads/cesiumjs/releases/1.126/Build/Cesium/Cesium.js"></script>
-
-  <link href="https://cesium.com/downloads/cesiumjs/releases/1.126/Build/Cesium/Widgets/widgets.css" rel="stylesheet">
-
-</head>
-<style>/* hide cesium elements *//*
-.cesium-viewer-animationContainer, .cesium-viewer-timelineContainer, .cesium-viewer-bottom, .cesium-viewer-fullscreenContainer, .cesium-viewer-toolbar {
-  display: none !important;*/
-}</style>
-<body>
-
-  <div id="cesiumContainer"></div>
-
-  <script type="module">
-    // Your access token can be found at: https://ion.cesium.com/tokens.
-    // Replace 'your_access_token' with your Cesium ion access token.
-
-
-    Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2MzE1YjcxNS0yZjA5LTQyMjgtYTgxMy1kNTJkYWEyNDczM2QiLCJpZCI6Mjc5OTg2LCJpYXQiOjE3NDA3NDY0MTF9.e8NfSN05dzDMqasqzxwgNE3JI1cPfrcCiSDnmaKLbug';
-
-    // Initialize the Cesium Viewer in the HTML element with the 'cesiumContainer' ID.
-    const viewer = new Cesium.Viewer('cesiumContainer', {
-      terrain: Cesium.Terrain.fromWorldTerrain(),
-    });    
-
-    // Fly the camera to San Francisco at the given longitude, latitude, and height.
-    viewer.camera.flyTo({
-      destination: Cesium.Cartesian3.fromDegrees(7, 45, 1500),
-      orientation: {
-        heading: Cesium.Math.toRadians(0.0),
-        pitch: Cesium.Math.toRadians(-15.0),
-      }
-    });
-
-    // Add Cesium OSM Buildings, a global 3D buildings layer.
-    const buildingTileset = await Cesium.createOsmBuildingsAsync();
-    viewer.scene.primitives.add(buildingTileset); 
-    const circle = new Cesium.CircleGeometry({
-  center : Cesium.Cartesian3.fromDegrees(-75.59777, 40.03883),
-  radius : 1000000.0
-});
-const geometry = Cesium.CircleGeometry.createGeometry(circle);
-  </script>
-
- </div>
-
-</body>
-
-</html>
-`);
+  res.send();
 });
 app.get("/accueil/", (req, res) => {
   res.render("accueil", { name: "utilisateur" });
