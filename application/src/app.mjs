506a376b2705057b07cb6ee51830d6e91cf1c8b1
antoineFabr <163823977+antoineFabr@users.noreply.github.com>
2025-03-07 15:56:14 +0100

    🚧frontend with VueJs searchBar

--- application/src/app.mjs
+++ application/src/app.mjs
@@ -20,6 +20,12 @@ app.set("view engine", "ejs");
 
 });
 */
+app.use((req, res, next) => {
+  res.append("Access-Control-Allow-Origin", ["*"]);
+  res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.append("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(express.json());
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
