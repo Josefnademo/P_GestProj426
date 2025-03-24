c8c537cea715c4e2ea74c5616fc62fa69e7cab4a
YOSEF NADEMO <72689921+Josefnademo@users.noreply.github.com>
2025-03-24 13:21:29 +0100

    logo officiel; header[almost done] il reste css; footer[almost done]
    logo officiel; header[almost done] il reste css; footer[almost done]

---
+++ yosef/frontend/unesco/src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer>
+    <ul>
+      <li>INFOS & RESERVATIONS</li>
+      <li>+41 79 417 71 55</li>
+      <li>ccroset@acd-motorsport.ch</li>
+    </ul>
+    <a href="HomePage.vue"
+      ><img src="./Logos/logo-ACD.png" alt="logo ACD motorsport"
+    /></a>
+  </footer>
+</template>
---
+++ yosef/frontend/unesco/src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <div class="Logo">
+      <a href="HomePage.vue"
+        ><img src="./Logos/logo-ACD.png" alt="logo ACD motorsport"
+      /></a>
+    </div>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <a href="accueil.ejs">Accueil</a>
+          </li>
+          <li>
+            <a href="login.ejs">Login</a>
+          </li>
+          <li>
+            <a href="register.ejs">Register</a>
+          </li>
+          <li>
+            <a href="account.ejs">Account</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
