b7a32e390f417e54606fbd71d1a020e658f474ca
YOSEF NADEMO <72689921+Josefnademo@users.noreply.github.com>
2025-03-31 13:09:30 +0200

    pages, css, routes, structure du projet(dossiers)
    merge, all modifications; from "yosef" to improve, complete the  main code in fronted. + j'ai fait(routes, AccountPage, DetailsPage,LoginPage,footer.css,header.css(modifié, ajusté),Footer.vue, header.vue(modifié, ajusté),structure du projet(dossiers))

--- frontend/unesco/src/router/index.js
+++ frontend/unesco/src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import home from "../components/home.vue";
 import about from "../components/about.vue";
+import LoginPage from "../views/LoginPage.vue";
+import DetailsPage from "../views/DetailsPage.vue";
+import AccountPage from "../views/AccountPage.vue";
 
 const routes = [
   {
@@ -11,6 +14,21 @@ const routes = [
   {
     path: "/search",
     component: about,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: LoginPage,
+  },
+  {
+    path: "/details",
+    name: "Details",
+    component: DetailsPage,
+  },
+  {
+    path: "/account",
+    name: "Account",
+    component: AccountPage,
   },
 ];
 
