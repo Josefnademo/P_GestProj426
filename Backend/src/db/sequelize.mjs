64d23bbaa37d6a9b16b634be423478ffac38466d
Antoine Piguet <pn01kbc@eduvaud.ch>
2025-03-31 13:16:11 +0200

    rajout(sequelize models)
    ajout simple des fichiers supprimés, ils ne sont pas encore utilisé par le code donc pas d'erreur pour l'instant.

---
+++ Backend/src/db/sequelize.mjs
@@ -0,0 +1,23 @@
+import { Sequelize, DataTypes } from "sequelize";
+import { CompteModel } from "../models/compte.mjs";
+
+import config from "../config.mjs";
+
+// Configuration de la connexion Sequelize
+const sequelize = new Sequelize(
+  config.db_name,
+  config.db_user,
+  config.db_password,
+  {
+    host: config.db_host,
+    port: config.db_port,
+    dialect: config.db_dialect,
+    logging: config.sequelize_log,
+  }
+);
+
+// Initialisation du modèle Product
+const compte = CompteModel(sequelize, DataTypes);
+
+// Exporter les ressources nécessaires
+export { sequelize, compte };
