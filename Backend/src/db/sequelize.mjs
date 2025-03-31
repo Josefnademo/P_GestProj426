ef1a5f3385f4ec85e5691cc046c55af3639c8635
Antoine Piguet <pn01kbc@eduvaud.ch>
2025-03-31 13:39:03 +0200

    adapt(sequelize)
    j'ai adapté le fichier sequelize pour pouvoir utiliser les models depuis n'importe quel fichiers.

--- Backend/src/db/sequelize.mjs
+++ Backend/src/db/sequelize.mjs
@@ -1,23 +1,55 @@
 import { Sequelize, DataTypes } from "sequelize";
-import { CompteModel } from "../models/compte.mjs";
-
-import config from "../config.mjs";
+import dbConfig from "../config.mjs";
+//models
+import AvisModel from "../models/avis.mjs";
+import CompteModel from "../models/compte.mjs";
+import LieuModel from "../models/lieu.mjs";
+import PaysModel from "../models/pays.mjs";
+import RegionModel from "../models/region.mjs";
+import ResideModel from "../models/resider.mjs";
+import EtreSitueDansModel from "../models/situeDans.mjs";
+import VisiterModel from "../models/visit.mjs";
+import AimeraitVisiterModel from "../models/wantToVisit.mjs";
 
 // Configuration de la connexion Sequelize
 const sequelize = new Sequelize(
-  config.db_name,
-  config.db_user,
-  config.db_password,
+  dbConfig.db_name,
+  dbConfig.db_user,
+  dbConfig.db_password,
   {
-    host: config.db_host,
-    port: config.db_port,
-    dialect: config.db_dialect,
-    logging: config.sequelize_log,
+    host: dbConfig.db_host,
+    port: dbConfig.db_port,
+    dialect: dbConfig.db_dialect,
+    logging: dbConfig.sequelize_log,
   }
 );
+const Avis = AvisModel(sequelize, DataTypes);
 
-// Initialisation du modèle Product
-const compte = CompteModel(sequelize, DataTypes);
+const Compte = CompteModel(sequelize, DataTypes);
 
-// Exporter les ressources nécessaires
-export { sequelize, compte };
+const Lieu = LieuModel(sequelize, DataTypes);
+
+const Pays = PaysModel(sequelize, DataTypes);
+
+const Region = RegionModel(sequelize, DataTypes);
+
+const Resider = ResideModel(sequelize, DataTypes);
+
+const EtreSitueDans = EtreSitueDansModel(sequelize, DataTypes);
+
+const Visiter = VisiterModel(sequelize, DataTypes);
+
+const AimeraitVisiter = AimeraitVisiterModel(sequelize, DataTypes);
+
+export {
+  sequelize,
+  Avis,
+  Compte,
+  Lieu,
+  Pays,
+  Region,
+  Resider,
+  EtreSitueDans,
+  Visiter,
+  AimeraitVisiter,
+};
