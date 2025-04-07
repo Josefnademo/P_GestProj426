468e6d3402fe4e332883350f62c6ed3e173665c4
Romain Pierre Marie Denis <ps04egl@eduvaud.ch>
2025-04-07 13:17:41 +0200

    Format(Profile Page)

--- Backend/src/routes/profile.mjs
+++ Backend/src/routes/profile.mjs
@@ -6,28 +6,28 @@ import crypto from "crypto";
 
 const profileRouter = express.Router();
 
-//This code is literally the same as the one in login but it returns information
+//Get the profile of the user with the id in the url 
 profileRouter.get("/:id", async (req, res) => {
-  const token = req.headers["authorization"]?.split(" ")[1];
+  const token = req.headers["authorization"]?.split(" ")[1];          //Get JWT token
   if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
   }
   try {
-    const decoded = jwt.verify(token, config.private_key);
-    const username = decoded.username;
-    const connection = await mysql.createConnection(config.dbConfig);
-    const [results] = await connection.execute(
-      "SELECT * FROM t_compte WHERE username = ?",
-      [username]
-    );
-    if (results.length === 0) {
-      return res.status(404).json({ message: "User not found" });
+    const decoded = jwt.verify(token, config.private_key);            //Check token and find user of the token
+    const username = decoded.username;                                //
+    const connection = await mysql.createConnection(config.dbConfig); //
+    const [results] = await connection.execute(                       //
+      "SELECT * FROM t_compte WHERE username = ?",                    //
+      [username]                                                      //
+    );                                                                
+    if (results.length === 0) {                                       //If user not found or token doesnt correspond 
+      return res.status(400).json({ message: "User not found" });     //
     } else {
       const user = results[0];
-      return res.status(200).json({
-        id: user.id,
-        username: user.username,
-        hashedPassword: user.hashedPassword,
+      return res.status(200).json({                                   //If token is fine and user exists, return information 
+        id: user.id,                                                  //
+        username: user.username,                                      //
+        hashedPassword: user.hashedPassword,                          //
       });
     }
   } catch (err) {
@@ -36,39 +36,37 @@ profileRouter.get("/:id", async (req, res) => {
   }
 });
 
+//PATCH the user with arguments in the request
+//This request can be executed with a new username, a new password, or both
 profileRouter.patch("/:id", async (req, res) => {
-  const token = req.headers["authorization"]?.split(" ")[1];
+  const token = req.headers["authorization"]?.split(" ")[1];          //Get token from header
   if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
   }
   try {
-    const decoded = jwt.verify(token, config.private_key);
-    const username = decoded.username;
-    const { newUsername, newPassword } = req.body;
-
-    const connection = await mysql.createConnection(config.dbConfig);
-    const [results] = await connection.execute(
-      "SELECT * FROM t_compte WHERE username = ?",
-      [username]
+    const decoded = jwt.verify(token, config.private_key);            //Check token and find user of the token        
+    const username = decoded.username;                                //
+    const { newUsername, newPassword } = req.body;                    //
+    const connection = await mysql.createConnection(config.dbConfig); //
+    const [results] = await connection.execute(                       //
+      "SELECT * FROM t_compte WHERE username = ?",                    //
+      [username]                                                      //
     );
-
-    if (results.length === 0) {
-      return res.status(404).json({ message: "User not found" });
+    if (results.length === 0) {                                       //If user not found or token doesnt correspond 
+      return res.status(404).json({ message: "User not found" });     //
     } else {
-      const user = results[0];
-      const salt = user.salt;
+      const user = results[0];                                        //Put user in variable
+      const salt = user.salt;                                         //Put salt in variable
 
-      let updates = [];
-      let values = [];
+      let updates = [];                                               //Empty array for the updates
+      let values = [];                                                //Empty array for the values
 
-      // Puts in the parameters the new values if they are not undefined or null
-      // Does this for everything
-      if (newUsername && typeof newUsername === "string") {
-        updates.push("username = ?");
-        values.push(newUsername);
+      if (newUsername && typeof newUsername === "string") {           //If user is an argument in the request
+        updates.push("username = ?");                                 //Add the argument of the username to the command that will be run
+        values.push(newUsername);                                     //Add the value of the username to the command that will be run
       }
 
-      if (newPassword && typeof newPassword === "string") {
+      if (newPassword && typeof newPassword === "string") {           //If password is an argument in the request
         const newHash = await new Promise((resolve, reject) => {
           crypto.pbkdf2(newPassword, salt, 1000, 64, "sha256", (err, key) => {
             if (err) reject(err);
@@ -76,26 +74,18 @@ profileRouter.patch("/:id", async (req, res) => {
           });
         });
         if (newHash) {
-          updates.push("hashedPassword = ?");
-          values.push(newHash);
+          updates.push("hashedPassword = ?");                           //Add the argument of the password to the command that will be run
+          values.push(newHash);                                         //Add the value of the password to the command that will be run
 
-          updates.push("salt = ?");
-          values.push(salt);
+          updates.push("salt = ?");                                     //Add the argument of the salt to the command that will be run
+          values.push(salt);                                            //Add the value of the salt to the command that will be run
         }
       }
-
       if (updates.length === 0) {
         console.log("No fields to update");
         return res.status(400).json({ message: "No fields to update" });
       }
-
-      values.push(user.compte_id);
-
-      console.log(
-        "Query:",
-        `UPDATE t_compte SET ${updates.join(", ")} WHERE compte_id = ?`
-      );
-      console.log("Values:", values);
+      values.push(user.compte_id);                                      //Add the id of the user to the command that will be run
 
       if (values.some((value) => value === undefined || value === null)) {
         console.error(
@@ -107,18 +97,20 @@ profileRouter.patch("/:id", async (req, res) => {
         });
       }
 
-      const query = `UPDATE t_compte SET ${updates.join(
-        ", "
-      )} WHERE compte_id = ?`;
+      const query = `UPDATE t_compte SET ${updates.join(                //Joins the command with the arguments and values previously defined
+        ", "                                                            //
+      )} WHERE compte_id = ?`;                                          //
 
-      const [updateResult] = await connection.execute(query, values);
+      const [updateResult] = await connection.execute(query, values);   //Runs the command
 
-      let newToken = token; // Default token c'est le vieux
-      if (newUsername) {
-        newToken = jwt.sign({ username: newUsername }, config.private_key, {
-          expiresIn: "1h",
-        });
-      }
+      let newToken = token;                                             //Default to the old token, becuase user is not nessesaraly changing  
+
+      if (newUsername) {                                                //If user is changing, make new JWT
+        newToken = jwt.sign({ username: newUsername },                  //
+          config.private_key, {                                         //
+          expiresIn: "1h",                                              //
+        });                                                             //
+      }                                                                 //
 
       return res.status(200).json({
         message: "Profile updated successfully",
@@ -131,35 +123,37 @@ profileRouter.patch("/:id", async (req, res) => {
   }
 });
 
-//WIP not done yet
+//DELETE a user
 profileRouter.delete("/:id", async (req, res) => {
-  const token = req.headers["authorization"]?.split(" ")[1];
+  const token = req.headers["authorization"]?.split(" ")[1];            //Get token from header
   if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
   }
-  try {
-    const decoded = jwt.verify(token, config.private_key);
-    const username = decoded.username;
-    const { id } = req.params;
-
-    const connection = await mysql.createConnection(config.dbConfig);
-    const [results] = await connection.execute(
-      "SELECT * FROM t_compte WHERE username = ?",
-      [username]
-    );
+  try {                                                 
+    const decoded = jwt.verify(token, config.private_key);              //Verify token and find user of the token
+    const username = decoded.username;                                  //
+    const { id } = req.params;                                          //
+    const connection = await mysql.createConnection(config.dbConfig);   //
+    const [results] = await connection.execute(                         //
+      "SELECT * FROM t_compte WHERE username = ?",                      //
+      [username]                                                        //
+    );                                                                  //
 
     if (results.length === 0) {
       return res.status(404).json({ message: "User not found" });
     } else {
       const user = results[0];
 
-      if (parseInt(id, 10) !== user.compte_id) {
-        return res.status(403).json({ message: "Forbidden: ID mismatch" });
+      if (parseInt(id, 10) !== user.compte_id) {                        
+        return res.status(403).json({
+          message: "Forbidden: ID mismatch"                       
+        });
       }
 
-      await connection.execute("DELETE FROM t_compte WHERE compte_id = ?", [
-        user.compte_id,
-      ]);
+      await connection.execute(                                           //Run the command to delete user
+        "DELETE FROM t_compte WHERE compte_id = ?", [                     //
+        user.compte_id,                                                   //
+      ]);                                                                 //
 
       return res.status(200).json({ message: "User deleted successfully" });
     }
